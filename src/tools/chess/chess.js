f0ae926450b95089308296ab193a280cd6657ab2
313224646 <18620800179@163.com>
2019-08-26 01:22:49 +0800

    获取棋子

--- src/tools/chess/chess.js
+++ src/tools/chess/chess.js
@@ -1,129 +1,57 @@
 const CHESS = require('./chess.json')
-let profession = {
-  FaShi: [],
-  CiKe: [],
-  JianShi: [],
-  DouShi: [],
-  YuanSuShi: [],
-  HuWei: [],
-  QiangShou: [],
-  QiShi: [],
-  YouXia: [],
-  HuanXingShi: []
+let PROFESSION = {
+  法师: [],
+  刺客: [],
+  剑士: [],
+  斗士: [],
+  元素使: [],
+  护卫: [],
+  枪手: [],
+  骑士: [],
+  游侠: [],
+  换形师: []
 }
-let race = {
-  EMo: [],
-  Long: [],
-  LangRen: [],
-  JiDi: [],
-  DiGuo: [],
-  RenZhe: [],
-  GuiZu: [],
-  AnYing: [],
-  HaiDao: [],
-  HaiKeSi: [],
-  JiQiRen: [],
-  XuKong: [],
-  KuangYe: [],
-  YueDeErRen: []
+let RACE = {
+  恶魔: [],
+  龙: [],
+  浪人: [],
+  极地: [],
+  帝国: [],
+  忍者: [],
+  贵族: [],
+  暗影: [],
+  海盗: [],
+  海克斯: [],
+  机器人: [],
+  虚空: [],
+  狂野: [],
+  约德尔人: []
 }
 
 // 划分职业和种族
 CHESS.forEach(item => {
   item.profession.split('-').forEach(value => {
-    switch (value) {
-      case '法师':
-        profession.FaShi.push(item)
-        break
-      case '刺客':
-        profession.CiKe.push(item)
-        break
-      case '剑士':
-        profession.JianShi.push(item)
-        break
-      case '斗士':
-        profession.DouShi.push(item)
-        break
-      case '元素使':
-        profession.YuanSuShi.push(item)
-        break
-      case '护卫':
-        profession.HuWei.push(item)
-        break
-      case '枪手':
-        profession.QiangShou.push(item)
-        break
-      case '骑士':
-        profession.QiShi.push(item)
-        break
-      case '游侠':
-        profession.YouXia.push(item)
-        break
-      case '换形师':
-        profession.HuanXingShi.push(item)
-        break
-      default:
-        console.log('profession: ', item.profession)
-        throw '无法识别棋子的职业'
-        break
+    if (PROFESSION[value]) {
+      PROFESSION[value].push(item)
+    } else {
+      console.log('profession: ', item.profession)
+      throw '无法识别棋子的职业'
     }
   })
-  
+
   item.race.split('-').forEach(value => {
-    switch (value) {
-      case '恶魔':
-        race.EMo.push(item)
-        break
-      case '龙':
-        race.Long.push(item)
-        break
-      case '浪人':
-        race.LangRen.push(item)
-        break
-      case '极地':
-        race.JiDi.push(item)
-        break
-      case '帝国':
-        race.DiGuo.push(item)
-        break
-      case '忍者':
-        race.RenZhe.push(item)
-        break
-      case '贵族':
-        race.GuiZu.push(item)
-        break
-      case '暗影':
-        race.AnYing.push(item)
-        break
-      case '海盗':
-        race.HaiDao.push(item)
-        break
-      case '机器人':
-        race.JiQiRen.push(item)
-        break
-      case '海克斯':
-        race.HaiKeSi.push(item)
-        break
-      case '虚空':
-        race.XuKong.push(item)
-        break
-      case '狂野':
-        race.KuangYe.push(item)
-        break
-      case '约德尔人':
-        race.YueDeErRen.push(item)
-        break
-      default:
-        console.log('race: ',item.race)
-        throw '无法识别棋子的种族'
-        break
+    if (RACE[value]) {
+      RACE[value].push(item)
+    } else {
+      console.log('race: ', item.race)
+      throw '无法识别棋子的种族'
     }
   })
 })
 
 // 锁定数据
-Object.freeze(profession)
-Object.freeze(race)
+Object.freeze(PROFESSION)
+Object.freeze(RACE)
 
 class Chess {
   /**
@@ -133,19 +61,70 @@ class Chess {
   constructor(chess) {
     this.name = chess.name
     this.race = chess.race
-    this.career = chess.career
-    this.price = this.price
-    this.numbering = this.numbering
-    this.avatar = this.avatar
+    this.profession = chess.profession
+    this.price = chess.price
+    this.numbering = chess.numbering
+    this.avatar = chess.avatar
   }
 
-  getProfessionChess () { // 获取其他职业棋子
+  getProfessionChess () { // 获取同职业棋子
   }
 
   getRaceChess () { // 获取其他同种族棋子
   }
 }
 
-function pinyin (value) { // 转成拼音
-  value
+// 获取某职业的棋子或所有职业的棋子
+function getProfession (profession) {
+  let professionAll = []
+  if (profession) {
+    if (PROFESSION[profession]) {
+      return PROFESSION[profession].map(item => {
+        return new Chess(item)
+      })
+    } else {
+      throw `不存在该职业(${profession})的棋子`
+    }
+  } else {
+    for (const key in PROFESSION) {
+      if (PROFESSION.hasOwnProperty(key)) {
+        professionAll.push(
+          PROFESSION[key].map(item => {
+            return new Chess(item)
+          })
+        ) 
+      }
+    }
+    return professionAll
+  }
+}
+
+// 获取某种族的棋子或所有种族的棋子
+function getRace (race) {
+  let raceAll = []
+  if (race) {
+    if (RACE[race]) {
+      return RACE[race].map(item => {
+        return new Chess(item)
+      })
+    } else {
+      throw `不存在该职业(${race})的棋子`
+    }
+  } else {
+    for (const key in RACE) {
+      if (RACE.hasOwnProperty(key)) {
+        raceAll.push(
+          RACE[key].map(item => {
+            return new Chess(item)
+          })
+        ) 
+      }
+    }
+    return raceAll
+  }
+}
+
+export {
+  getProfession,
+  getRace
 }
